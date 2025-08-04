d8e91359821edad2d7ae1c73d88000a1629fc9da
andresanchessouto <andresanchessouto@gmail.com>
2025-08-04 15:53:01 -0300

    fix: config layput

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { App } from './app';
+import { Home } from './pages/home/home';
+import { Login } from './pages/authentication/login/login';
+import { MainLayout } from './layouts/main-layout/main-layout';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        component: MainLayout,
+        children: [{
+            path: "home",
+            component: Home
+        }]
+    },
+    {
+        path: "login",
+        component: Login
+    }
+];
